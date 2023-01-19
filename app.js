05bbbe779f633adcde5ac32d78e3e44457a3f37d
saulwade <saulwade29@gmail.com>
2023-01-18 22:31:25 -0600

    finished javascript code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const box = document.querySelector(".box"),
 inputPart = box.querySelector(".inputp"),
 infoText = inputPart.querySelector(".infotext"),
-inputField = inputPart.querySelector("input");
-locationButton = inputPart.querySelector(".get-device");
+inputField = inputPart.querySelector("input"),
+locationButton = inputPart.querySelector(".get-device"),
+wicon = box.querySelector(".weatherbox img"),
+buttonBack = box.querySelector(".back-to-menu")
 
 let api;
 
@@ -22,7 +24,7 @@ locationButton.addEventListener("click", ()=> {
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords; 
-    api = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=20dbed72930a4933821b3c8b83bda1ad';
+    api = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metrics&appid=20dbed72930a4933821b3c8b83bda1ad';
     fetchData();
 }
 
@@ -32,7 +34,7 @@ function onError(error){
 }
 
 function requestApi(city){
-    api = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=20dbed72930a4933821b3c8b83bda1ad'; 
+    api = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metricsappid=20dbed72930a4933821b3c8b83bda1ad'; 
     fetchData();
 }
 
@@ -43,5 +45,43 @@ function fetchData (){
 }
 
 function weatherDetails(info){
-    console.log(info);
+    infoText.classList.replace("pendiente", "error");
+    if(info.cod == "404"){
+        infoText.innerText = '${inputField.value} isnt a valid city name';
+    }else{
+        const city = info.name;
+        const country = info.sys.country;
+        const {description, id} = info.weather[0];
+        const {feels_like, humidity, temp} = info.main;
+
+        if(id == 800){
+            wicon.src = "img/soleado.svg";
+        }else  if(id >= 200 && id <= 232){
+            wicon.src = "img/tormenta.svg";
+        }else  if(id >= 600 && id <= 622){
+            wicon.src = "img/nieve.svg";
+        }else  if(id >= 701 && id <= 781){
+            wicon.src = "img/aire.svg";
+        }else  if(id >= 801 && id <= 804){
+            wicon.src = "img/nublado.svg";
+        }else  if((id >= 300 && id <= 321) || (id >= 500 && id <= 531)){
+            wicon.src = "img/lluvia.svg";
+        }
+        
+
+        box.querySelector(".temperatura .numero").innerText = Math.floor(temp);
+        box.querySelector(".weather").innerText = description;
+        box.querySelector(".location span").innerText = '${city}, ${country}';
+        box.querySelector(".temperatura .numero2").innerText = feels_like;
+        box.querySelector(".humidity span").innerText = '${humidity}%';
+
+
+        infoText.classList.remove("pendiente", "error");
+        box.classList.add("active");
+        console.log(info);
+    }
 }
+
+buttonBack.addEventListener("click", ()=>{
+    box.classList.remove("active");
+})
